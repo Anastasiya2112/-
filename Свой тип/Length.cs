e17361cc17a87b3022d3ec97d782e26fb6b343dc
Anastasiya P <anastasiyaap18@gmail.com>
2022-11-22 20:40:35 +0800

    Добавлен код первой части, есть ошибка

--- Свой тип/Length.cs
+++ Свой тип/Length.cs
@@ -88,5 +88,80 @@ namespace Свой_тип
         {
             return instance / number;
         }
+
+        public Length To(MeasureType newType)
+        {
+            // по умолчанию новое значение совпадает со старым
+            var newValue = this.value;
+            // если текущий тип -- это градусы цельсия
+            if (this.type == MeasureType.C)
+            {
+                switch (newType)
+                {
+                    // если конвертим в метр, то значение не меняем
+                    case MeasureType.C:
+                        newValue = this.value;
+                        break;
+                    // если в км.
+                    case MeasureType.F:
+                        newValue = this.value / ((9 / 5) + 32);
+                        break;
+                    // если в  а.е.
+                    case MeasureType.Ra:
+                        newValue = this.value / (1.8 + 491.67);
+                        break;
+                    // если в парсек
+                    case MeasureType.K:
+                        newValue = this.value / 273.15;
+                        break;
+                }
+            }
+            else if (newType == MeasureType.C) // если новый тип: метр
+            {
+                switch (this.type)
+                {
+                    // если конвертим в C, то значение не меняем
+                    case MeasureType.C:
+                        newValue = this.value;
+                        break;
+                    // если в F.
+                    case MeasureType.F:
+                        newValue = this.value * ((9 / 5) + 32);
+                        break;
+                    // если в  Ra
+                    case MeasureType.Ra:
+                        newValue = this.value * (1.8 + 491.67);
+                        break;
+                    // если в K
+                    case MeasureType.K:
+                        newValue = this.value * 273.15;
+                        break;
+                }
+            }
+            else // то есть не в метр и не из метра
+            {
+                newValue = this.To(MeasureType.C).To(newType).value;
+            }
+            return new Length(newValue, newType);
+
+
+
+        } //тут что-то не то
+
+        public static Length operator +(Length instance1, Length instance2)
+        {
+            // то есть у текущей длине добавляем число 
+            // полученное преобразованием значения второй длины в тип первой длины
+            // так как у нас определен operator+(Length instance, double number)
+            // то это сработает как ожидается
+            return instance1 + instance2.To(instance1.type).value;
+        }
+
+        // вычитание двух длин
+        public static Length operator -(Length instance1, Length instance2)
+        {
+            // тут все тоже, только с минусом
+            return instance1 - instance2.To(instance1.type).value;
+        }
     }
 }
--- Свой типTests/LengthTests.cs
+++ Свой типTests/LengthTests.cs
@@ -39,7 +39,7 @@ namespace Свой_тип.Tests
         {
             var length = new Length(3, MeasureType.F);
             length = length - 1.75;
-            Assert.AreEqual("1.25 F", length.Verbose());
+            Assert.AreEqual("1,25 F", length.Verbose());
         }
 
         [TestMethod()]
@@ -57,5 +57,48 @@ namespace Свой_тип.Tests
             length = length / 3;
             Assert.AreEqual("1 K", length.Verbose());
         }
+
+        [TestMethod()]
+        public void MeterToAnyTest()
+        {
+            Length length;
+
+            length = new Length(1 * (9 / 5) + 32, MeasureType.C);
+            Assert.AreEqual("33 F", length.To(MeasureType.F).Verbose());
+
+            length = new Length(2 * 1.8 + 491.67, MeasureType.C);
+            Assert.AreEqual("495,27 Ra", length.To(MeasureType.Ra).Verbose());
+
+            length = new Length(3 + 273.15, MeasureType.C);
+            Assert.AreEqual("276,15 K", length.To(MeasureType.K).Verbose());
+        }
+        [TestMethod()]
+        public void AnyToMeterTest()
+        {
+            Length length;
+
+            length = new Length(1, MeasureType.F);
+            Assert.AreEqual("33 C", length.To(MeasureType.C).Verbose());
+
+            length = new Length(1, MeasureType.Ra);
+            Assert.AreEqual("493,47 C", length.To(MeasureType.C).Verbose());
+
+            length = new Length(1, MeasureType.K);
+            Assert.AreEqual("273,15 C", length.To(MeasureType.C).Verbose());
+        }
+
+            [TestMethod()]
+            public void AddSubKmMetersTest()
+            {
+                var Ce = new Length(100, MeasureType.C);
+                var Ke = new Length(1, MeasureType.K);
+
+                Assert.AreEqual("373,15 C", (Ce + Ke).Verbose());
+                Assert.AreEqual("101 K", (Ke + Ce).Verbose());
+
+                Assert.AreEqual("-99 K", (Ke - Ce).Verbose());
+                Assert.AreEqual("-173,15 C", (Ce - Ke).Verbose());
+            }
+        
     }
 }
